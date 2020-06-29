96291754a47a3e856a3d646ee261374cbf02dafb
Athanasios Liaskas <al3415@ic.ac.uk>
2020-06-29 16:33:55 +0300

    Added additinal Python Object Models from Github
    Found examples of python files using Python Object Models that I'm pushing to Git for reference purposes

--- Python-Examples/BQL Python Object Model - from Manual/main.py
+++ Python-Examples/BQL Python Object Model - from Manual/main.py
@@ -4,9 +4,12 @@ import bql
 # Create the database connection 
 bq = bql.Service()
 
-# The query pulling out the 20-day average price for SP500 members
+#The Object Model
+#===========================================
 
-# Define px as a price series for last 20 days
+# The python model pulling out the 20-day average price for SP500 members
+
+# Define px (i.e. price) as a price series for last 20 days
 px = bq.data.px_last(dates=bq.fumc.range('-20d', '0d'))
 # Define the avg_px by applying function avg to the time series px
 avg_px = bq.func.avg(px)
@@ -17,14 +20,15 @@ index_memb = bq.univ.members('SPX Index')
 # Create the request
 request = bql.Request(index_memb, {'avg_px': avg_px}, with_params={'currency': 'USD'})
 
-# Execute the rquiest with database conncection
+# Execute the request
 response = bq.execute(request)
 
 # combined_df os a utility function to convert response object into panda's DataFrame
 data = bql.combined_df(response)    
 
 
-# **Query String Versions** #
+# The String Interface
+#===========================================
 # The query pulling out the 20-day average price for SP500 members
 q_str = """
 let(#avg_px = avg(px_last(dates=range(-20d, 0d)));)
